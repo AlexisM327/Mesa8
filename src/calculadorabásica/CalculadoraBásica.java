deabf8a2bb4664b70834ff1f778f5a44b4911ae3
pc-24 <pc-24@Pc24>
2026-01-31 11:19:21 -0600

    Principal

---
+++ src/calculadorabásica/CalculadoraBásica.java
@@ -0,0 +1,18 @@
+package calculadorabásica;
+import java.util.Scanner;
+public class CalculadoraBásica {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner (System.in);
+    
+        System.out.println("Ingresa el primer numero: ");
+        int a = teclado.nextInt();
+        System.out.println("Ingresa el segundo numero: ");
+        int b = teclado.nextInt();
+        
+        //Suma suma = new Suma();
+        //Resta resta = new Resta();
+    
+        //System.out.println("La suma de los dos digitos es: " suma.sumar(a, b));
+        //System.out.println("La resta de los dos digitos es: " resta.retar(a, b));
+    
+    }}
